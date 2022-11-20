b96c6a5d005c390bf90fb254c06fdb05abab37d5
asmir <Asmir Kadusic>
2022-11-20 01:37:48 +0100

    2022/11/19, Adding small changes and trying to resolve problem with CMake (not successfully

--- server-side/include/ServerState.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <caf/all.hpp>
-
-caf::behavior ServerBehavior(caf::event_based_actor *);
--- server-side/src/ServerState.cpp
+++
@@ -1,8 +0,0 @@
-#include <ServerState.hpp>
-
-using namespace caf;
-
-behavior ServerBehavior(event_based_actor *) {
-  return {[](add_atom, int32_t a, int32_t b) { return a + b; },
-          [](sub_atom, int32_t a, int32_t b) { return a - b; }};
-}
